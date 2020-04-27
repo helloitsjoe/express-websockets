67f0c1be82317a4e221ec368917fa762cd14887f
helloitsjoe <jboyle617@gmail.com>
2020-04-26 20:02:10 -0400

    continuous polling

--- index.js
+++ index.js
@@ -5,74 +5,75 @@ const box = document.getElementById('box');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const button = document.getElementById('button');
+const username = document.getElementById('username');
 const heartbeat = document.getElementById('heartbeat');
 const connected = document.getElementById('connected');
 const closeButton = document.getElementById('close-button');
 
-// Make WebSocket connection to server. `window.location.hostname` will
-// work on both localhost or the IP address (for connecting a mobile device)
-const ws = new WebSocket(`ws://${window.location.hostname}:3000`);
+let messages = [];
+
+const fetchData = () => {
+  return fetch('/data')
+    .then(res => res.json())
+    .then(data => {
+      if (messages.length === data.length) {
+        return;
+      }
+
+      for (const { text, user, id } of data) {
+        console.log(id);
+        if (messages.find(m => m.id === id)) {
+          console.log(`Skipping`);
+          continue;
+        }
+
+        const isSelf = user === username.value;
+
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble', isSelf ? 'us' : 'them');
+        bubble.innerText = text;
+        box.appendChild(bubble);
+
+        if (!isSelf) {
+          const info = document.createElement('p');
+          info.classList.add('info');
+          info.innerText = `SENT FROM USER ${user}`;
+          box.appendChild(info);
+        }
+      }
+      messages = data;
+    });
+};
+
+const poll = () => {
+  setTimeout(() => {
+    fetchData().then(() => {
+      poll();
+    });
+  }, 1000);
+};
+
+fetchData();
+poll();
 
 // Focus the message on page load
 input.focus();
-
-// Called when the connection opens
-ws.onopen = e => {
-  closeButton.disabled = false;
-  closeButton.onclick = () => ws.close();
-};
-
-// Called every time a message is received from the server
-ws.onmessage = message => {
-  const { type, isSelf, text, clientId } = JSON.parse(message.data);
-
-  // This is one way to handle different types of messages. Socket.io
-  // provides an abstraction that allows you to avoid this.
-  switch (type) {
-    case 'message': {
-      const bubble = document.createElement('div');
-      bubble.classList.add('bubble', isSelf ? 'us' : 'them');
-      bubble.innerText = text;
-      box.appendChild(bubble);
-
-      if (!isSelf) {
-        const info = document.createElement('p');
-        info.classList.add('info');
-        info.innerText = `SENT FROM USER ${clientId}`;
-        box.appendChild(info);
-      }
-      break;
-    }
-    case 'connection': {
-      connected.innerText = 'CONNECTED!';
-      break;
-    }
-    default:
-      console.warn('Unrecognized message type:', type);
-  }
-};
-
-// Called when either the client or server closes the connection
-ws.onclose = () => {
-  console.log(`Closing WebSocket...`);
-  connected.innerText = 'NOT CONNECTED';
-
-  const info = document.createElement('p');
-  info.classList.add('connection');
-  info.innerText = `DISCONNECTED`;
-  box.appendChild(info);
-};
 
 form.onsubmit = e => {
   e.preventDefault();
 
   const text = input.value;
+  const user = username.value;
 
   if (!text) {
     return;
   }
 
-  ws.send(JSON.stringify({ text }));
+  fetch('/data', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user, text }),
+  });
 
   input.value = '';
 };
--- server.js
+++ server.js
@@ -1,48 +1,27 @@
 const http = require('http');
-const path = require('path');
 const express = require('express');
-const WebSocket = require('ws');
+const bodyParser = require('body-parser');
 const ip = require('ip');
 
-// Set up Express, create a server using the Node http module,
-// and provide that server to WebSocket.Server
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+
+app.use(bodyParser.json());
 
 // This serves index.html and other assets from the current directory.
 app.use(express.static(__dirname));
 
+const messages = [];
 let uniqueId = 0;
 
-// Listen for clients to connect. The `ws` argument is the connected client.
-wss.on('connection', ws => {
-  // This is a simple way to assign a unique id to keep track of each client.
-  // We mutate the `ws` object so that all clients will know who the sender is.
-  ws.clientId = uniqueId++;
+app.get('/data', (req, res) => {
+  res.send(JSON.stringify(messages));
+});
 
-  // We could send simple string messages, but sending stringified objects
-  // makes it easy to send different types of messages and data payloads.
-  ws.send(JSON.stringify({ type: 'connection' }));
-
-  // Listen for messages from connected clients
-  ws.on('message', message => {
-    const { text } = JSON.parse(message);
-
-    // wss.clients is a Set() of all connected clients.
-    for (const client of wss.clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(
-          JSON.stringify({
-            type: 'message',
-            isSelf: client === ws,
-            clientId: ws.clientId,
-            text,
-          })
-        );
-      }
-    }
-  });
+app.post('/data', (req, res) => {
+  console.log(`req.body:`, req.body);
+  messages.push({ ...req.body, id: uniqueId++ });
+  res.send(JSON.stringify(messages));
 });
 
 server.listen(3000, () => {
